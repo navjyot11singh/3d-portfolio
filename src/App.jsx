64b6cdc877ef2a0e50d38e4bdc3d80ad9389b05b
Navjyot singh <32342012+navjyot11singh@users.noreply.github.com>
2024-09-28 00:49:47 -0400

    Commit for my 3d portfolio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
